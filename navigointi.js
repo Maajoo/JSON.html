cde1664a4463093223c67aa8d027f497081de242
Maajoo <mio.kantola@hotmail.com>
2024-09-27 15:28:10 +0300

    Tehtävä 4 ja css

--- navigointi.js
+++ navigointi.js
@@ -1,11 +1,13 @@
-// navigation.js
+// navigaatiota varten tehty Javascript
 function loadNavigation() {
     document.getElementById('nav').innerHTML = `
     <nav>
         <a href="kurssi.html">Kurssi</a>
         <a href="toteutus.html">Toteutus</a>
         <a href="tapahtumat.html">Tapahtumat</a>
-        <a href="saa.html">Saa</a>
+        <a href="saa.html">Sää</a>
+        <a href="liikennekamera.html">Liikennekamera</a>
+        <a href="junat.html">Junat</a>
     </nav>
     `;
 }
